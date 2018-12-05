855d89b0e70a5bb470acfe995875acbfce90ed70
honez <809564576@qq.com>
2018-12-05 13:38:21 +0800

    上传企业站源码

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+$mysqli=new Mysqli("localhost","root","root","test");
+if($mysqli->connect_error)
+{
+	die( $mysqli->connect_error);
+}
+$sql='select *from user';
+$res=$mysqli->query($sql);
+
+if($row=$res->fetch_assoc())
+{
+	foreach ($row as $key => $value) {
+		echo "$value";
+	}
+}
+?>
